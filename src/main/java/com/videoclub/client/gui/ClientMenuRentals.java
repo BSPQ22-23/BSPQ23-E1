5bb9b5506676cf6155b2ba9e4dde5964c78802b8
Alar Encinas Teijido <117914356+EncinasAlar@users.noreply.github.com>
2023-04-28 15:34:28 +0200

    gui

---
+++ src/main/java/com/videoclub/client/gui/ClientMenuRentals.java
@@ -0,0 +1,133 @@
+package com.videoclub.client.gui;
+
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.text.html.parser.Entity;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import com.videoclub.client.gui.ClientMenuWindow.LogInWindow;
+import com.videoclub.pojo.User;
+
+
+public class ClientMenuRentals extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7388753432723633814L;
+	private JPanel contentPane;
+	private JPanel panelSouth;
+	private JPanel panelCentre;
+	private JLabel lblOldPass;
+	private JTextField txtOldPass;
+	private JLabel lblNewPass;
+	private JTextField textPass;
+	private JButton btnSave;
+	private JButton btnBack;
+	private static final String SERVER_ENDPOINT = "http://localhost:8080/webapi";
+    private static final String USERS_RESOURCE ="users";
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ClientMenuRentals frame = new ClientMenuRentals();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	
+	
+
+	/**
+	 * Create the frame.
+	 */
+	public ClientMenuRentals() {
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		panelSouth = new JPanel();
+		contentPane.add(panelSouth, BorderLayout.SOUTH);
+		
+		btnBack = new JButton("BACK");
+		panelSouth.add(btnBack);
+		
+		panelCentre = new JPanel();
+		contentPane.add(panelCentre, BorderLayout.CENTER);
+		panelCentre.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		
+		textPass = new JPasswordField();
+		panelCentre.add(textPass);
+		textPass.setColumns(10);
+		
+		
+		
+		
+
+
+		btnBack.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						Thread hilo = new MenuWindow();
+						hilo.start();
+						dispose();
+						}
+					
+				});
+
+	}
+	class MenuWindow extends Thread{
+		public void run() {
+			ClientMenuWindow.main(null);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
