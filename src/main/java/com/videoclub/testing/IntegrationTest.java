0b9a97201f5bc2a1ea6866b5c036f4a914110dbd
Alar Encinas Teijido <117914356+EncinasAlar@users.noreply.github.com>
2023-05-03 19:47:00 +0200

    Update IntegrationTest.java

--- src/main/java/com/videoclub/testing/IntegrationTest.java
+++ src/main/java/com/videoclub/testing/IntegrationTest.java
@@ -3,10 +3,13 @@ package com.videoclub.testing;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
@@ -18,9 +21,11 @@ import org.junit.Test;
 import com.videoclub.dao.MovieDAO;
 import com.videoclub.dao.RentalDAO;
 import com.videoclub.dao.UserDAO;
+import com.videoclub.encrypt.PasswordEncrypt;
 import com.videoclub.pojo.Movie;
 import com.videoclub.pojo.Rental;
 import com.videoclub.pojo.User;
+import com.videoclub.pojo.typeUser;
 
 public class IntegrationTest {
 
@@ -28,8 +33,13 @@ private static final String SERVER_ENDPOINT = "http://localhost:8080/webapi";
 private static final String USERS_RESOURCE ="users";
 private static final String RENTALS_RESOURCE ="rentals";
 private static final String MOVIES_RESOURCE ="movies";
-	
-	
+private int usercode;
+private String movietitle;
+private int Rentalid;
+Date currentDate = new Date();
+User usertest = new User("test",PasswordEncrypt.encryptPassword("test"), "test@gmail.com", "test", "test", typeUser.CLIENT );
+Movie movietest = new Movie("test", "test", 100, 2021, "test", 15);
+Rental rentaltest = new Rental(movietest, usertest,currentDate , currentDate);
 	@Test
 	public void UserListResource() {
 		List<User> users= new ArrayList<>();
@@ -58,15 +68,34 @@ private static final String MOVIES_RESOURCE ="movies";
 	
 	@Test
 	public void AddUserResource() {
-		
-		//TODO
+		Client client = ClientBuilder.newClient();
+        final WebTarget appTarget = client.target(SERVER_ENDPOINT);
+		User usertest = new User("test",PasswordEncrypt.encryptPassword("test"), "test@gmail.com", "test", "test", typeUser.CLIENT );
+        Response response = appTarget.path(USERS_RESOURCE)
+            .request(MediaType.APPLICATION_JSON)
+            .post(Entity.entity(usertest, MediaType.APPLICATION_JSON)
+        );
+        
+        User userCod = response.readEntity(User.class);
+        usercode=userCod.getCode();
+        // check if the response was ok
+        assertEquals(response.getStatusInfo().toEnum(),Status.OK);
+        
 	}
 	
 	
 	@Test
 	public void DeleteUserResource() {
 		
-		//TODO
+		Client client = ClientBuilder.newClient();
+        final WebTarget appTarget = client.target(SERVER_ENDPOINT);
+		 Response response = appTarget.path(USERS_RESOURCE)
+	                .path(Integer.toString(usercode))
+	                .request()
+	                .delete();
+		 
+		 assertEquals(response.getStatusInfo().toEnum(),Status.OK);
+        
 	}
 	
 	
@@ -99,13 +128,31 @@ private static final String MOVIES_RESOURCE ="movies";
 	@Test
 	public void AddMovieResource() {
 		
-		//TODO
+		Client client = ClientBuilder.newClient();
+        final WebTarget appTarget = client.target(SERVER_ENDPOINT);
+        Response response = appTarget.path(MOVIES_RESOURCE)
+            .request(MediaType.APPLICATION_JSON)
+            .post(Entity.entity(movietest, MediaType.APPLICATION_JSON)
+        );
+        
+        Movie movieTit = response.readEntity(Movie.class);
+        movietitle=movieTit.getTitle();
+        // check if the response was ok
+        assertEquals(response.getStatusInfo().toEnum(),Status.OK);
 	}
 	
 	
 	@Test
 	public void DeleteMovieResource() {
-		//TODO
+		
+		Client client = ClientBuilder.newClient();
+        final WebTarget appTarget = client.target(SERVER_ENDPOINT);
+		 Response response = appTarget.path(MOVIES_RESOURCE)
+	                .path(movietitle)
+	                .request()
+	                .delete();
+		 
+		 assertEquals(response.getStatusInfo().toEnum(),Status.OK);
 	
 	}
 	
@@ -138,14 +185,31 @@ private static final String MOVIES_RESOURCE ="movies";
 	@Test
 	public void AddRentalResource() {
 		
-		//TODO
+		Client client = ClientBuilder.newClient();
+        final WebTarget appTarget = client.target(SERVER_ENDPOINT);
+        Response response = appTarget.path(USERS_RESOURCE)
+            .request(MediaType.APPLICATION_JSON)
+            .post(Entity.entity(rentaltest, MediaType.APPLICATION_JSON)
+        );
+        
+        Rental rentalID = response.readEntity(Rental.class);
+        Rentalid=rentalID.getId();
+        // check if the response was ok
+        assertEquals(response.getStatusInfo().toEnum(),Status.OK);
 	}
 	
 	
 	@Test
 	public void DeleteRentalResource() {
 		
-	//TODO
+		Client client = ClientBuilder.newClient();
+        final WebTarget appTarget = client.target(SERVER_ENDPOINT);
+		 Response response = appTarget.path(USERS_RESOURCE)
+	                .path(Integer.toString(Rentalid))
+	                .request()
+	                .delete();
+		 
+		 assertEquals(response.getStatusInfo().toEnum(),Status.OK);
 	}
 
 }
